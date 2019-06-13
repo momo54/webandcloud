d98c1d79228da2f7aecae814d55c2a5efe73c303
Pascal MOLLI <molli-p@nfsrootha0.ensinfo.sciences.univ-nantes.prive>
2019-06-13 18:05:15 +0200

    PAgination petition

---
+++ myapp2018/src/main/java/foo/PetitionQuery.java
@@ -0,0 +1,75 @@
+package foo;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.EntityNotFoundException;
+import com.google.appengine.api.datastore.FetchOptions;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.datastore.KeyRange;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.PropertyProjection;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.CompositeFilterOperator;
+import com.google.appengine.api.datastore.Query.FilterOperator;
+import com.google.appengine.api.datastore.Query.FilterPredicate;
+
+import com.google.appengine.repackaged.com.google.datastore.v1.CompositeFilter;
+import com.google.appengine.repackaged.com.google.datastore.v1.Projection;
+import com.google.appengine.repackaged.com.google.datastore.v1.PropertyFilter;
+
+@WebServlet(name = "PetQuery", urlPatterns = { "/pquery" })
+public class PetitionQuery extends HttpServlet {
+
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+		response.setContentType("text/html");
+		response.setCharacterEncoding("UTF-8");
+
+
+		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+		Key k = KeyFactory.createKey("PU", "P0");
+
+		Query q = new Query("PU").setFilter(new FilterPredicate(Entity.KEY_RESERVED_PROPERTY, FilterOperator.GREATER_THAN, k));
+
+		PreparedQuery pq = datastore.prepare(q);
+		List<Entity> result = pq.asList(FetchOptions.Builder.withLimit(10));
+
+		response.getWriter().print("<li> result:" + result.size() + "<br>");
+		Entity last=null;
+		for (Entity entity : result) {
+			response.getWriter().print("<li>" + entity.getKey());
+			last=entity;
+		}
+
+
+		q = new Query("PU").setFilter(new FilterPredicate("__key__", FilterOperator.GREATER_THAN, last.getKey()));
+
+		pq = datastore.prepare(q);
+		result = pq.asList(FetchOptions.Builder.withLimit(10));
+
+		response.getWriter().print("<li> result:" + result.size() + "<br>");
+		last=null;
+		for (Entity entity : result) {
+			response.getWriter().print("<li>" + entity.getKey());
+			last=entity;
+		}
+
+		
+	}
+}
---
+++ myapp2018/src/main/java/foo/PetitionServlet.java
@@ -0,0 +1,50 @@
+package foo;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.EntityNotFoundException;
+import com.google.appengine.api.datastore.FetchOptions;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.datastore.KeyRange;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.FilterOperator;
+import com.google.appengine.api.datastore.Query.FilterPredicate;
+
+@WebServlet(name = "PetServlet", urlPatterns = { "/petition" })
+public class PetitionServlet extends HttpServlet {
+
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+		response.setContentType("text/html");
+		response.setCharacterEncoding("UTF-8");
+
+		Random r = new Random();
+		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+
+		// Create users
+		for (int i = 0; i < 50; i++) {
+			for (int j = 0; j < 100; j++) {
+				Entity e = new Entity("PU", "P" + i + "_" + "U"+j);
+				e.setProperty("firstName", "My name is" + j);
+				datastore.put(e);
+			}
+		}
+	}
+}
